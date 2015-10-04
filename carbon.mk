4823989ef7751b115a25bf6c8b906fbd19982594
TechVeerA <techveera007@gmail.com>
2015-10-04 13:35:56 +0000

    carbon-ify

---
+++ carbon.mk
@@ -0,0 +1,6 @@
+$(call inherit-product, vendor/carbon/config/common_phone.mk)
+
+$(call inherit-product, device/samsung/kltespr/full_kltespr.mk)
+
+PRODUCT_DEVICE := kltespr
+PRODUCT_NAME := carbon_kltespr
